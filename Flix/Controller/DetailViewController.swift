d3003744d81aa44dcc2491edd0a72814efaae28c
Mihai Ruber <mruber@ucsd.edu>
2018-02-07 20:50:43 -0800

    Added the details vc

--- Flix/Controller/DetailViewController.swift
+++ Flix/Controller/DetailViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 enum MovieKeys {
     static let title = "title"
+    static let backdropPath = "backdrop_path"
+    static let posterPath = "poster_path"
 }
 
 class DetailViewController: UIViewController {
@@ -19,7 +21,9 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var releaseDateLabel: UILabel!
-    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UITextView!
+    
     
     // Variables
     var movie: [String : Any]?
@@ -29,8 +33,17 @@ class DetailViewController: UIViewController {
 
         if let movie = movie {
             titleLabel.text = movie[MovieKeys.title] as? String
-            releaseDateLabel.text = movie["relase_date"] as? String
+            releaseDateLabel.text = movie["release_date"] as? String
             overviewLabel.text = movie["overview"] as? String
+            let backdropPathString = movie[MovieKeys.backdropPath] as! String
+            let posterPathString = movie[MovieKeys.posterPath] as! String
+            let baseURLString = "https://image.tmdb.org/t/p/w500"
+            
+            let backdropURL = URL(string: baseURLString + backdropPathString)!
+            backDropImageView.af_setImage(withURL: backdropURL)
+            let posterPathURL = URL(string: baseURLString + posterPathString)!
+            posterImageView.af_setImage(withURL: posterPathURL)
+            
         }
         
         // Do any additional setup after loading the view.
--- Flix/Controller/HomeViewController.swift
+++ Flix/Controller/HomeViewController.swift
@@ -93,6 +93,16 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
             return UITableViewCell()
         }
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell) {
+            let movie = movies[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.movie = movie
+        }
+        
+    }
 
 }
 
