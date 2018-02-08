435473e1a17c0c6146f09b5e10d578eefee798a8
Mihai Ruber <mruber@ucsd.edu>
2018-02-07 21:38:16 -0800

    added vc

---
+++ Flix/Model/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Mihai Ruber on 2/7/18.
+//  Copyright © 2018 Mihai Ruber. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+}
